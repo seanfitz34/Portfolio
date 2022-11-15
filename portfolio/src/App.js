09b4d55bda30c0ec43d3a7b1910ee5b3a1348656
seanfitz34 <seanwilliamfitzgerald@gmail.com>
2022-11-14 18:04:10 -0800

    updating starter code

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,10 @@
+import Profile from "./PortfolioContainer/Home/Profile";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Profile/>
+  </div>;
 }
 
 export default App;
--- portfolio/src/PortfolioContainer/Home/Profile.js
+++ portfolio/src/PortfolioContainer/Home/Profile.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
+import Typical from "react-typical";
 
 export default function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <div className="profile-contianer">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://github.com/seanfitz34">
+              <i className="fa fa-github"></i>
+            </a>
+
+            <a href="https://www.instagram.com/studiocitysean/">
+              <i className="fa fa-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/seanfitzgerald34/">
+              <i className="fa fa-linkedin"></i>
+            </a>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {""}
+              Hello, I'm <span className="highlighted-text">Sean</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {""}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 🟢",
+                    1000,
+                    "Full Stack Developer 💻",
+                    1000,
+                    "MERN Stack Dev 😎",
+                    1000,
+                    "Cross Platform Dev 🌐",
+                    1000,
+                    "React/React Native Dev 📱",
+                    1000,
+                  ]}
+                />
+              </h1>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
