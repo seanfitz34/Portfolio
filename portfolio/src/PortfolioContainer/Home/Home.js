6d2a85846f5521eee65dbd33290ff786cf528579
seanfitz34 <seanwilliamfitzgerald@gmail.com>
2022-11-17 17:42:48 -0800

    updating starter code

--- portfolio/src/PortfolioContainer/Home/Home.js
+++ portfolio/src/PortfolioContainer/Home/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Profile from "./Profile/Profile";
 import Footer from "./Footer/Footer";
-import Nav from "./NavBar/NavBar"
-import About from "./AboutMe/AboutMe"
+import Nav from "./NavBar/NavBar";
+import About from "./AboutMe/AboutMe";
+import Resume from "./Resume/Resume";
 import "./Home.css";
 
 export default function Home() {
   return (
     <div className="home-container" id="home">
-      <Nav/>
+      <Nav />
       <Profile />
       <Footer />
-      <About/>
+      <About />
+      <Resume />
+  
     </div>
   );
 }
